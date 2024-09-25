7f4b7ada4049b4d769df3a966e0ee13d6f0adf77
hemunguia <hmunguia@byui.edu>
2024-09-25 08:18:38 -0600

    Loops Pgm

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string response;
+        do
+        {
+            Random randomGenerator = new();
+            int number = randomGenerator.Next(1, 100);
+            int magicN;
+            do
+            {
+                Console.WriteLine("What is the magic number?");
+                string magicNumber = Console.ReadLine();
+                magicN = int.Parse(magicNumber);
+
+                if (magicN > number) 
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (magicN < number) 
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+             while (magicN != number);
+            
+            Console.WriteLine("You guessed it!");
+            
+            Console.WriteLine("Do you want to continue? ");
+            response = Console.ReadLine();
+
+
+        } 
+        while (response == "yes");
+        
     }
 }
