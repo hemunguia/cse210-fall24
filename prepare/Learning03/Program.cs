220c13e7a3bc46d5ba505f277b7a7e6e10634284
hemunguia <hmunguia@byui.edu>
2024-10-19 09:36:25 -0600

    W05 Prepare: Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionstring());
+        Console.WriteLine(fraction.GetDecimalValue());
+        
+        Fraction fraction1 = new Fraction(5);
+        Console.WriteLine(fraction1.GetFractionstring());
+        Console.WriteLine(fraction1.GetDecimalValue());
+        
+        Fraction fraction2 = new Fraction(3,4);
+        Console.WriteLine(fraction2.GetFractionstring());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(1,3);
+        Console.WriteLine(fraction3.GetFractionstring());
+        Console.WriteLine(fraction3.GetDecimalValue());
+       
+
+
     }
 }
