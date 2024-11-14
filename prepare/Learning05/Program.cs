a073a7311c9c63f5bd1b26d1bc0d066f15ffa668
hemunguia <hmunguia@byui.edu>
2024-11-13 21:07:11 -0700

    W09 Learning Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("White", 15);
+        shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("Black", 15,24);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("Brown", 55);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color {color} has an area of {area}.");
+        }
+
+
+
+
+
+
     }
 }
